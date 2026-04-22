68d8e97bbd221c6419f2838589f63ac8f89f2367
Hasan Malik <hasanimranmalik245@gmail.com>
2026-04-22 10:01:35 -0400

    tools

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from pydantic_ai import Agent
 from pydantic import BaseModel
+import datetime
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -8,14 +10,27 @@ class CountryData(BaseModel):
     capital: str
     population: int
 
+def get_todays_date():
+    """Return today's date as a YYYY-MM-DD string."""
+    return datetime.date.today().isoformat()
+
 agent = Agent(
     model='anthropic:claude-haiku-4-5-20251001',
-    system_prompt="You are an assistant. Answer concisely." # system prompt: sent with every user prompt
+    system_prompt="You are an assistant. Answer concisely.", # system prompt: sent with every user prompt
+    tools=[get_todays_date]
 )
+
+print("Welcome!")
 
 result = agent.run_sync(
     user_prompt="Liechtenstein",
     output_type=CountryData
+)
+
+print(result.output)
+
+result = agent.run_sync(
+    user_prompt="How many days till April 23?",
 )
 
 print(result.output)
