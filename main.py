ad5f6d65ac04a4b29098fc2391c4194017676cd7
Hasan Malik <hasanimranmalik245@gmail.com>
2026-04-22 13:52:24 -0400

    dependencies

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pydantic_ai import Agent
+from pydantic_ai import Agent, RunContext
 from pydantic import BaseModel
 import datetime
 
@@ -10,15 +10,22 @@ class CountryData(BaseModel):
     capital: str
     population: int
 
-def get_todays_date():
+def get_todays_date() -> str:
     """Return today's date as a YYYY-MM-DD string."""
     return datetime.date.today().isoformat()
 
 agent = Agent(
     model='anthropic:claude-haiku-4-5-20251001',
     system_prompt="You are an assistant. Answer concisely.", # system prompt: sent with every user prompt
-    tools=[get_todays_date]
+    tools=[get_todays_date],
+    deps_type=str
 )
+
+@agent.system_prompt  
+def append_to_system_prompt(ctx: RunContext[str]) -> str:
+    """The string returned by this function will be appended to the system prompt."""
+    return f"The user's name is {ctx.deps}. Address them by their name."
+
 
 print("Welcome!")
 
@@ -31,6 +38,7 @@ print(result.output)
 
 result = agent.run_sync(
     user_prompt="How many days till April 23?",
+    deps="Malik"
 )
 
 print(result.output)
