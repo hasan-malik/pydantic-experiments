5ab089a313b47d5df336e483248df9c1bf6b8832
Hasan Malik <hasanimranmalik245@gmail.com>
2026-04-22 09:55:47 -0400

    configured BaseModel

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from pydantic_ai import Agent
+from pydantic import BaseModel
 from dotenv import load_dotenv
 load_dotenv()
+
+class CountryData(BaseModel):
+    name: str
+    capital: str
+    population: int
 
 agent = Agent(
     model='anthropic:claude-haiku-4-5-20251001',
     system_prompt="You are an assistant. Answer concisely." # system prompt: sent with every user prompt
 )
 
-result = agent.run_sync(user_prompt="What is the capital of Bulgaria?")
+result = agent.run_sync(
+    user_prompt="Liechtenstein",
+    output_type=CountryData
+)
+
 print(result.output)
