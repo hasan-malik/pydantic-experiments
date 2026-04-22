8af1cfae58a985631ce68d7a9d53751a62d2a3b8
Hasan Malik <hasanimranmalik245@gmail.com>
2026-04-22 09:19:39 -0400

    gitignore

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from pydantic_ai import Agent
+from dotenv import load_dotenv
+load_dotenv()
 
 agent = Agent(
     model='openai:gpt-4o-mini',
-    system_prompt="You are an assistant. Answer concisely."
+    system_prompt="You are an assistant. Answer concisely." # system prompt: sent with every user prompt
 )
+
+agent.run_sync(user_prompt="What is the capital of Bulgaria?")
